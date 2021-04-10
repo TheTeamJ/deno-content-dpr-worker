caf0957d96f9098e85ae17de16f1a8d3ef551ccb
daiiz <da.iz.mg@gmail.com>
2021-04-11 00:01:15 +0900

    test: "Content-DPR" is included in response headers.

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,6 +1,14 @@
 import { assertEquals } from 'https://deno.land/std@0.92.0/testing/asserts.ts'
 import { handleRequest } from '../index.ts'
 
-Deno.test('"Content-DPR" is included in response headers', async () => {
-  console.log(handleRequest)
+const imageUrls = {
+  retina: 'https://raw.githubusercontent.com/TheTeamJ/deno-content-dpr-worker/main/sample/retina_pancake.png'
+}
+
+Deno.test('"Content-DPR" is included in response headers.', async () => {
+  const res = await handleRequest(imageUrls.retina)
+  const headers = res.headers
+  assertEquals(headers.get('Content-DPR'), '2')
+  assertEquals(headers.get('X-Width'), '1102')
+  assertEquals(headers.get('X-Height'), '994')
 })
